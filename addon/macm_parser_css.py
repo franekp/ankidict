a1546b65f7731f65e25e064cead55ed1d25daaba
Franek Piszcz <franek@kompfranka>
2014-11-26 11:29:17 +0100

    added support for css selectors starting with ">" or " >"

--- addon/macm_parser_css.py
+++ addon/macm_parser_css.py
@@ -14,27 +14,44 @@ def parse_func(txt):
 		return lxml.html.fromstring(txt)
 
 def select_func(self,query):
-	# TODO TODO ogar czy soup dopuszcza select siebie, jeśli nie(najprawdopodobniej), to
-	# TODO TODO trzea zrobić to tak, że opakowujemy go i potem jedziemy z selectem.
+	#1 [DONE] ogar czy soup dopuszcza select siebie, jeśli nie(najprawdopodobniej), to
+	#1 [DONE] trzea zrobić to tak, że opakowujemy go (bierzemy 'parent')
+	# 		i potem jedziemy z selectem.
+	#2 [DONE] zrobić dobre sel_css(">...")
+	#3 TODO przepisać i sprawdzić cały kod, żeby się zgadzał z xpathem
+	#4 ew. koniec query typu txt i to jakoś jeszcze... (ale raczej nie)
 	id_obscure = "asdfgh1234"
+	while query[0] == ' ':
+		query = query[1:]
 	if(USE_SOUP):
 		if(query[0] != '>'):
 			return self.select(query)
 		else:
-			tmp_id = self['id']
-			self['id'] = id_obscure
-			res = self.select("#"+id_obscure+" "+query)
-			self['id'] = tmp_id
+			tmp_id = None
+			if 'id' in self.attrs:
+				tmp_id = self.attrs['id']
+			self.attrs['id'] = id_obscure
+			# tutaj jest "parent", bo wygląda na to, że nie można zaznaczyć samego siebie
+			res = self.parent.select("#"+id_obscure+" "+query)
+			if tmp_id:
+				self.attrs['id'] = tmp_id
+			else:
+				del self.attrs['id']
 			return res
 	else:
 		#TODO: precompiling queries and caching them in some dict
 		if(query[0] != '>'):
 			return self.cssselect(query)
 		else:
-			tmp_id = self.attrib['id']
+			tmp_id = None
+			if 'id' in self.attrib:
+				tmp_id = self.attrib['id']
 			self.attrib['id'] = id_obscure
 			res = self.cssselect("#"+id_obscure+" "+query)
-			self.attrib['id'] = tmp_id
+			if tmp_id:
+				self.attrib['id'] = tmp_id
+			else:
+				del self.attrib['id']
 			return res
 
 
@@ -67,9 +84,9 @@ class word_sense(object):
 		./span[@class='SENSE-VARIANT']//span[@class='BASE']/text() | \
 		./span[@class='MULTIWORD']//span[@class='BASE']/text()")
 		'''
-		self.keys = element.sel_css("strong")
-		self.keys += element.sel_css("span.SENSE-VARIANT span.BASE")
-		self.keys += element.sel_css("span.MULTIWORD span.BASE")
+		self.keys = element.sel_css(" > strong")
+		self.keys += element.sel_css(" > span.SENSE-VARIANT span.BASE")
+		self.keys += element.sel_css(" > span.MULTIWORD span.BASE")
 		self.keys = map_get_text(self.keys)
 		self.keys = self.keys + ks
 		def mk_example(el):
@@ -145,6 +162,7 @@ class dict_entry(object):
 		def get_nested(a):
 			return a.sel_css("div.SUB-SENSE-CONTENT")
 		nsense_bodies = [ [i]+get_nested(i) for i in sense_bodies]
+		# flatten sense_bodies
 		sense_bodies = [item for sublist in nsense_bodies for item in sublist]
 		self.senses = map(lambda a: word_sense(a) , sense_bodies)
 		#related:
