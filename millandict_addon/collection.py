b0f328b570b11ed339228375a01b30c027bd5780
Wojciech Kordalski <wojtek.kordalski@gmail.com>
2014-12-03 22:24:49 +0100

    Repaired Anki interface
    Now cards are added to correct deck.
    The UI of Anki is refreshed after adding card.

--- millandict_addon/collection.py
+++ millandict_addon/collection.py
@@ -10,18 +10,19 @@ def select_deck():
   self = get_plugin()
   deckname = self.config.deck
   deckid = mw.col.decks.id(deckname)
-  # selects deck - why? [TODO]
-  # maby we can only add notes to selected deck...
-  mw.col.decks.select(deckid)
-  # gets deck object - why?
-  deck = mw.col.decks.get(deckid)
-  return deck
-
+  # choosing deck
+  # we only need to set in the model
+  # I do not know why
+  # Code got from tests
+  m = mw.col.models.current()
+  m['did'] = deckid
+  mw.col.models.save(m)
+  
 
 def add_note(q, a):
   # adds card to collection
   # [TODO] test
-  deck = select_deck()
+  select_deck()
 
   note = get_note(q)
   if note != None:
@@ -33,7 +34,7 @@ def add_note(q, a):
     note['Front'] = q
     note['Back'] = a
     mw.col.addNote(note)
-    #deck.addNote(note)
+    mw.reset()
 
 def add_tag(q, t):
   # adds tag to note
@@ -44,15 +45,14 @@ def add_tag(q, t):
   if not note: raise "Najpierw otaguj siebie, zanim zaczniesz tagować to co nie istnieje..."
   note.tags.append(t)
   note.flush()
+  mw.reset()
 
 def get_note(q):
   # returns card with selected question
   # [TODO] test
   select_deck()
   notes = mw.col.findNotes('"'+re.escape(q)+'"')
-  print notes
   for note in [mw.col.getNote(n) for n in notes]:
-    print note
     if note['Front'] == q: return note
   return None
 
