ef691bfbb9b30515934b96b24b78ee386bf03eae
Franciszek Piszcz <franek.piszcz@gmail.com>
2015-04-02 12:16:05 +0200

    Added some comments to dict_window.py

--- millandict_addon/dict_window.py
+++ millandict_addon/dict_window.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-
 
-# PyQt library
+####
+# Copyright (c) 2014 Wojciech Kordalski
+# Copyright (c) 2014 Franciszek Piszcz
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+####
+
+
 from aqt.qt import *
 import aqt.qt as QtGui
 from PyQt4 import QtCore
@@ -24,7 +47,12 @@ from __init__ import get_plugin
 
 # TODO:
 
+# - TO THINK: adding some possibility for user to group synonyms (this would be done inside the Anki...)
+
+# - the CTRL-F feature
+
 # - add proper closing of word list log file
+# - add exception catching in the dict_query
 
 # - add better key phrase removal from the definitions (e.g. remove also words with 's'/'ed' at the end or phrasal verbs)
 #		encapsulate it in separate class/module/whatever and replace existing code with it
