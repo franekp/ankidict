5775dc8f86bb7aad93259f436c5c41356e80c57f
Franciszek Piszcz <franek@qed.ai>
2016-08-16 22:26:06 +0200

    Add gradientfixer css postprocessor.

--- ankidict/addon/run.sh
+++ ankidict/addon/run.sh
@@ -3,5 +3,5 @@ killall anki
 set -e  # exit the script if any of the following commands fails
 tidy -errors -q review.html  # html linter
 sass style.sass style.css  # sass preprocessor
-postcss -u autoprefixer style.css -o style.css  # autoprefixer
+postcss --use autoprefixer --use postcss-gradientfixer --replace style.css   # autoprefixer
 anki 2>&1 > /dev/null &
