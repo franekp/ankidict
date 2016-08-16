691318d26c2f40ecd83f05632f0725961709a028
Franciszek Piszcz <franek@qed.ai>
2016-08-16 22:15:40 +0200

    Add css autoprefixer.

--- ankidict/addon/run.sh
+++ ankidict/addon/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 killall anki
 set -e  # exit the script if any of the following commands fails
-tidy -errors -q review.html
-sass style.sass style.css
+tidy -errors -q review.html  # html linter
+sass style.sass style.css  # sass preprocessor
+postcss -u autoprefixer style.css -o style.css  # autoprefixer
 anki 2>&1 > /dev/null &
