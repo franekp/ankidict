ca9cba649393fdf1198c5fb9a12c9b9e8d01b895
Franciszek Piszcz <franek@qed.ai>
2016-11-01 22:32:51 +0100

    Add related words list.

--- ankidict/server/elmapp/Dictionary.elm
+++ ankidict/server/elmapp/Dictionary.elm
@@ -9,75 +9,109 @@ import Json.Decode as Json exposing ((:=))
 import Task
 
 -- MODEL
-
+type alias Link = {
+    key : String, url : String, link_type : String, part_of_speech : String
+  }
 type alias Sense = {definition : String, key : String, examples : List String}
-type alias Model = {word : String, dict_entry : List Sense}
+type alias DictEntry = {senses : List Sense, links : List Link}
+type alias ModelImpl a = {a | word : String}
+type Tab = SensesT | LinksT
+type Model = Model String DictEntry Tab
 type Action
   = SearchWord
-  | FetchSucceed (List Sense)
+  | FetchSucceed DictEntry
   | FetchFail Http.Error
   | WordChanged String
+  | ShowTab Tab
 
 init : (Model, Cmd Action)
 init =
-  (Model "make" [Sense "Welcome to our addon!" "" []], get_dict_entry "make")
+  (
+    Model "make" {
+      senses = [Sense "Welcome to our addon!" "" []], links = []
+    } SensesT,
+    get_dict_entry "make"
+  )
 
 -- UPDATE
 
 update : Action -> Model -> (Model, Cmd Action)
 update action model =
-  case action of
-    SearchWord ->
-      ({model | dict_entry = [Sense "Loading..." "" []]}, get_dict_entry model.word)
-
-    FetchSucceed new_entry ->
-      (Model model.word new_entry, Cmd.none)
-
-    FetchFail err ->
-      ({model | dict_entry = [Sense ("ERROR " ++ toString err) "" []]}, Cmd.none)
-
-    WordChanged new_word ->
-      ({model | word = new_word}, Cmd.none)
+  case model of
+    Model word dict_entry tab->
+    case action of
+      SearchWord ->
+        (Model word {dict_entry | senses = [Sense "Loading..." "" []]} tab, get_dict_entry word)
+      FetchSucceed new_entry ->
+        (Model word new_entry tab, Cmd.none)
+      FetchFail err ->
+        (Model word {dict_entry | senses = [Sense ("ERROR " ++ toString err) "" []]} tab, Cmd.none)
+      WordChanged new_word ->
+        (Model new_word dict_entry tab, Cmd.none)
+      ShowTab t ->
+        (Model word dict_entry t, Cmd.none)
 
 -- VIEW
 
 view : Model -> Html Action
 view model =
-  H.div [Att.class "dictionary"] [
-      H.header [] [
-        H.form [Att.action "#", Ev.onSubmit SearchWord] [
-          H.b [] [H.text "Macmillan Dictionary"],
-          H.button [Att.type' "submit"] [H.text "Search"],
-          H.div [] [
-            H.input [
-              Att.type' "text",
-              Att.value model.word,
-              Ev.onInput WordChanged
-            ] []
-          ]
-        ],
-        H.ul [] [
-          H.li [Att.class "active"] [H.text "Definition"],
-          H.li [] [H.text "Related words"]
-        ]
-      ],
-      H.section [] [
-        H.ol [] (
-          List.map (\sense ->
-            H.li [] [
-              (
-                if sense.key /= ""
-                  then H.b [] [H.text <| sense.key ++ " - "]
-                  else H.text ""
-              ),
-              H.text sense.definition,
-              H.ul [] (List.map (\example ->
-                H.li [] [H.text example]
-              ) sense.examples)
+  case model of
+    Model word dict_entry tab ->
+      H.div [Att.class "dictionary"] [
+          H.header [] [
+            H.form [Att.action "#", Ev.onSubmit SearchWord] [
+              H.b [] [H.text "Macmillan Dictionary"],
+              H.button [Att.type' "submit"] [H.text "Search"],
+              H.div [] [
+                H.input [
+                  Att.type' "text",
+                  Att.value word,
+                  Ev.onInput WordChanged
+                ] []
+              ]
+            ],
+            H.ul [] [
+              H.li (
+                  if tab == SensesT
+                    then [Att.class "active"]
+                    else [Ev.onClick <| ShowTab SensesT]
+              ) [H.text "Definition"],
+              H.li (
+                if tab == LinksT
+                  then [Att.class "active"]
+                  else [Ev.onClick <| ShowTab LinksT]
+              ) [H.text "Related words"]
             ]
-          ) model.dict_entry
+          ],
+          (case tab of
+            SensesT -> H.section [] [
+              H.ol [] (
+                List.map (\sense ->
+                  H.li [] [
+                    (
+                      if sense.key /= ""
+                        then H.b [] [H.text <| sense.key ++ " - "]
+                        else H.text ""
+                    ),
+                    H.text sense.definition,
+                    H.ul [] (List.map (\example ->
+                      H.li [] [H.text example]
+                    ) sense.examples)
+                  ]
+                ) dict_entry.senses
+              )
+            ]
+            LinksT -> H.section [] [
+              H.ol [] [
+                H.li [] ([H.b [] [H.text "Related words"], H.ul [] (List.map (\link ->
+                  H.li [] [
+                    H.b [] [H.text <| link.key ++ " "],
+                    H.span [Att.class "gray"] [H.text link.part_of_speech]
+                  ]
+                ) dict_entry.links
+              )])]
+            ]
         )
-      ]
     ]
 
 -- HTTP
@@ -87,7 +121,7 @@ get_dict_entry word =
   let
     url = "/api/dictionary?word=" ++ word
   in
-    Task.perform FetchFail FetchSucceed (Http.get decode_dict_entry url)
+    Task.perform FetchFail FetchSucceed (Http.get decode_senses url)
 
 -- JSON PARSING
 
@@ -101,11 +135,23 @@ maybestring m = case m of
   Nothing -> ""
   Just a -> a
 
-decode_dict_entry : Json.Decoder (List Sense)
-decode_dict_entry = ("senses" := Json.list (
+decode_senses : Json.Decoder DictEntry
+decode_senses =  Json.object2
+  (\senses -> \links -> {senses = senses, links = links})
+  ("senses" := Json.list (
     Json.object3
       (\def -> \key -> \ex -> Sense def (maybestring key) (maybelist ex))
       ("definition" := Json.string)
       (Json.maybe <| "original_key" := Json.string)
       (Json.maybe <| "examples" := Json.list ("content" := Json.string))
+  ))
+  ("links" := Json.list (
+    Json.object4
+      (\key -> \url -> \link_type -> \part_of_speech ->
+        Link key url link_type part_of_speech
+      )
+      ("key" := Json.string)
+      ("url" := Json.string)
+      ("link_type" := Json.string)
+      ("part_of_speech" := Json.string)
   ))
